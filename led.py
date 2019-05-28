b53bd8cde62e60cd790efaadf7d6d7aee843be58
Tristan <admin@tristans-air.lan>
2019-05-28 15:19:04 -0700

    Added more code so that there are now two pairs of LEDs (4)

--- led.py
+++ led.py
@@ -5,24 +5,31 @@ GPIO.setmode(GPIO.BCM)
 
 blinkCount = 3
 count = 0
-LEDPin = 22
-LEDPin2 = 23
+yellow = 22
+green = 17
+red = 12
+blue = 21
 
-# Setup the pin that the LED is connected to 
-GPIO.setup(LEDPin, GPIO.OUT)
-GPIO.setup(LEDPin2, GPIO.OUT)
-
+# Setup the pin that the LED is connected to
+GPIO.setup(yellow, GPIO.OUT)
+GPIO.setup(green, GPIO.OUT)
+GPIO.setup(red, GPIO.OUT)
+GPIO.setup(blue, GPIO.OUT)
 try:
-	while count < blinkCount:
-		GPIO.output(LEDPin, True)
-		GPIO.output(LEDPin2, False)
-		print("LED ON")
-		sleep(3)
-		GPIO.output(LEDPin, False)
-		GPIO.output(LEDPin2, True)
-		print("LED OFF")
-		sleep(3)
-		count += 1
+        while count < blinkCount:
+                GPIO.output(yellow, True)
+                GPIO.output(green, False)
+		GPIO.output(red, True)
+		GPIO.output(blue, False)
+                print("GROUP 1 ON")
+                sleep(2)
+                GPIO.output(yellow, False)
+                GPIO.output(green, True)
+		GPIO.output(red, False)
+		GPIO.output(blue, True)
+                print("Group 2 ON")
+                sleep(2)
+                count += 1
 
 finally:
-	GPIO.cleanup()
+        GPIO.cleanup()
